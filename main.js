2ad7e43d97c13d5227231f692adc1bb114cd79f4
faxaferganskiy <fahruzbekgulomov3@gmail.com>
2022-10-28 14:36:00 +0500

    init

---
+++ main.js
@@ -0,0 +1,44 @@
+const elForm = document.querySelector(".form")
+const elInput = document.querySelector(".input")
+const elList = document.querySelector(".list")
+
+const localData = JSON.parse(window.localStorage.getItem("todos"))
+
+const todos = localData || []
+
+let count = 1
+
+elForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const inputValue = elInput.value
+
+    let todo = {
+        name: inputValue,
+        id: count++
+    }
+    todos.push(todo)
+    window.localStorage.setItem("todos", JSON.stringify(todos))
+    elList.innerHTML = null
+    renderTodos()
+})
+
+const renderTodos = function() {
+    for(item of todos) {
+        const newButton = document.createElement("button")
+        const newLi = document.createElement("li")
+        const newP = document.createElement("p")
+        const newDiv = document.createElement("div")
+        newLi.textContent = item.name
+        newP.textContent = item.author
+        newButton.textContent = Delete
+
+        newDiv.setAttribute("class","div")
+        newP.setAttribute("class","desc")
+        elList.appendChild(newDiv)
+        newDiv.appendChild(newLi)
+        newDiv.appendChild(newP)
+        newDiv.appendChild(newButton)
+    }
+}
+renderTodos(todos, elList)
